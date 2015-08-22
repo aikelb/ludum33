20b93f242e1f04f89ad0b386cc6c42b47e767260
Armando <armandovmj@gmail.com>
2015-08-22 18:57:39 +0100

    script de pollito

---
+++ unity-ludum/Assets/Scripts/Chick.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Chick : MonoBehaviour {
+
+    public static event FXManager.FxEvent OnDeath;
+
+    void ReceiveDamage() {
+        //GetComponent<Animator>().Play("Death");
+        SendMessageUpwards("SetChickensAngry", SendMessageOptions.RequireReceiver);
+        if (OnDeath != null) {
+            OnDeath(transform.position);
+        }
+        Destroy(this);
+    }
+}
