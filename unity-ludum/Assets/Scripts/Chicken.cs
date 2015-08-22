9c853046bfee27c1b0d27e85c28e757f447085f5
Aitor Lozano <aike89@gmail.com>
2015-08-22 14:29:38 +0100

    chicken code

---
+++ unity-ludum/Assets/Scripts/Chicken.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Chicken : MonoBehaviour {
+
+	public static event FXManager.FxEvent OnStep;
+
+	enum states {
+		Idle,
+		Wandering,
+		Chasing,
+		Angry
+	}
+	
+	states state;
+
+	IEnumerator SMC () {
+		while (true) {
+			yield return StartCoroutine(state.ToString());
+		}
+	}
+	
+	IEnumerator Idle () {
+		while (state == states.Idle) {
+			Debug.Log("Idle");
+			yield return 0;
+		}
+	}
+	
+}
---
+++ unity-ludum/Assets/Scripts/FXManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FXManager : MonoBehaviour {
+
+	public delegate void FxEvent(Vector3 position);
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
