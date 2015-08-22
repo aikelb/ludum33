3f2f7ec025bb59c67dfc794f53a27c657092feea
Aitor Lozano <aike89@gmail.com>
2015-08-22 13:08:22 +0100

    basic movement

--- unity-ludum/Assets/Scripts/Player.cs
+++ unity-ludum/Assets/Scripts/Player.cs
@@ -3,13 +3,31 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	Vector3 desiredOrientation;
+	Vector3 currentVelocity;
+	public float movementSpeed;
+
+	void Awake () {
+		desiredOrientation = currentVelocity = Vector3.zero;
 	}
 	
-	// Update is called once per frame
 	void Update () {
-	
+		ReadInput();
+		currentVelocity = currentVelocity * 0.8f + 0.2f * (desiredOrientation * movementSpeed * Time.deltaTime);
+		transform.Translate(currentVelocity);
+		desiredOrientation = Vector3.zero;
 	}
+	
+	void ReadInput () {
+		if (Input.GetKey(KeyCode.W))
+			desiredOrientation += Vector3.up;
+		if (Input.GetKey(KeyCode.S))
+			desiredOrientation += Vector3.down;
+		if (Input.GetKey(KeyCode.A))
+			desiredOrientation += Vector3.left;
+		if (Input.GetKey(KeyCode.D))
+			desiredOrientation += Vector3.right;
+		desiredOrientation.Normalize();
+	}
+	
 }
