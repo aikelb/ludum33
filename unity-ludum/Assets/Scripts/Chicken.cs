de956f0abb14994da2e5b00de8f122f88026b999
Aitor Lozano <aike89@gmail.com>
2015-08-22 16:21:30 +0100

    IA basica del pollo

--- unity-ludum/Assets/Scripts/Chicken.cs
+++ unity-ludum/Assets/Scripts/Chicken.cs
@@ -1,9 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Chicken : MonoBehaviour {
+public class Chicken : MovingEntity {
 
-	public static event FXManager.FxEvent OnStep;
+	public static event FXManager.FxEvent OnDeath;
 
 	enum states {
 		Idle,
@@ -14,31 +14,87 @@ public class Chicken : MonoBehaviour {
 	
 	states state;
 	
+	[SerializeField]
+	Transform player;
+	bool alert = false;
+	Vector3 myDesiredOrientation = Vector3.zero;
+	
+	public float attackDistance = 1f;
+	
+	[Header("Wandering Setings")]
+	public float desireOrientationChance = 0.1f;
+	public float idleChance = 0.5f;
+	
+	[Header("Chase Setings")]
+	public float chaseMaxDistance = 2f;
+	public float chaseSpeedModifier = 1.5f;
+	
+	[Header("Angry Setings")]
+	public float angrySpeedModifier = 2.5f;
+	
+	void OnValidate () {
+		player = FindObjectOfType<Player>().transform;
+	}
+	
 	void Start () {
 		StartCoroutine(SMC());
 	}
 
 	IEnumerator SMC () {
 		while (true) {
+			Debug.Log("Starting state: " + state);
 			yield return StartCoroutine(state.ToString());
 		}
 	}
 	
 	IEnumerator Idle () {
 		while (state == states.Idle) {
-			Debug.Log("Idle");
-			yield return 0;
+			yield return new WaitForSeconds(Random.Range(0.5f, 2f));
+			state = states.Wandering;
 		}
 	}
 	
 	IEnumerator Wandering () {
+		DesireOrientation();
 		while (state == states.Wandering) {
+			if (Random.value < desireOrientationChance)
+				DesireOrientation();	
+			
+			if (myDesiredOrientation == Vector3.zero) {
+				state = states.Idle;
+			} else if (Random.value < idleChance)
+				state = states.Idle;
+				
+			desiredOrientation = myDesiredOrientation;
+			Move();
 			yield return 0;
 		}
 	}
 	
+	void DesireOrientation () {
+		if (Random.value < 0.5f)
+			myDesiredOrientation += Vector3.up;
+		else if (Random.value < 0.75f)
+			myDesiredOrientation += Vector3.down;
+			
+		if (Random.value < 0.5f)
+			myDesiredOrientation += Vector3.left;
+		else if (Random.value < 0.75f)
+			myDesiredOrientation += Vector3.right;
+		
+		myDesiredOrientation.Normalize();
+	}
+	
 	IEnumerator Chasing () {
 		while (state == states.Chasing) {
+			Vector3 towardsPlayer = player.position - transform.position;
+			
+			myDesiredOrientation = towardsPlayer.normalized;
+			desiredOrientation = myDesiredOrientation;
+			Move(chaseSpeedModifier);
+			
+			if (!alert && towardsPlayer.magnitude > chaseMaxDistance)
+				state = states.Idle;
 			yield return 0;
 		}
 	}
@@ -46,6 +102,14 @@ public class Chicken : MonoBehaviour {
 	IEnumerator Angry () {
 		while (state == states.Angry) {
 			yield return 0;
+		}
+	}
+	
+	void OnTriggerEnter (Collider other) {
+		if (other.tag == "Player") {
+			if (state != states.Angry) {
+				state = states.Chasing;
+			}
 		}
 	}
 	
