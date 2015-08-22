77dbc6c8add0b2c263c6bb8b7d08d0ed6f1695d0
Armando <armandovmj@gmail.com>
2015-08-22 13:21:41 +0100

    save for pull

---
+++ unity-ludum/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+    public GameObject m_objetive;
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = m_objetive.transform.position;
+	}
+}
