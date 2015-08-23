150fd17414e9681d156dee9f5c2fa5b9ac2d96cd
Armando <armandovmj@gmail.com>
2015-08-23 16:22:58 +0100

    Escena de inicio

---
+++ unity-ludum/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeScene : MonoBehaviour {
+
+    public void OpenScene(string scene) {
+        Application.LoadLevel(scene);
+    }
+
+    public void ExitGame() {
+        Application.Quit();
+    }
+}
