c4c70d6917b4152b0c45fdb4d8e629fb21b70d06
Román <roman.diaz.leon@gmail.com>
2015-08-23 19:27:50 +0100

    Aniadida animacion de ataque, parece que todo funciona menos por un pequeño delay al cambiar de animacion.

--- unity-ludum/Assets/Scripts/Player.cs
+++ unity-ludum/Assets/Scripts/Player.cs
@@ -5,26 +5,33 @@ public class Player : MovingEntity {
 	
 	public static event FXManager.FxEvent OnDeath;
 
-	private Animator animatorPlayer;
+	private Animator playerAnimator;
 
 	private float xAxis;
 	private float yAxis;
 
-	private enum playerDirection {
+	private enum PlayerDirection {
 		isOnX,
 		isOnY
 	}
 
-	private playerDirection direction;
+	private enum PlayerViewDirection {
+		isViewUp,
+		isViewDown
+	}
+
+	private PlayerDirection direction;
+	private PlayerViewDirection viewDirection;
 
 	void Awake () {
 		desiredOrientation = currentVelocity = Vector3.zero;
-		animatorPlayer = GetComponent<Animator>();
+		playerAnimator = GetComponent<Animator>();
 	}
 	
 	void Update () {
 		ReadInput();
 		Move();
+		Attack();
 	}
 	
 	void ReadInput () {
@@ -34,8 +41,8 @@ public class Player : MovingEntity {
 
 		//Movimiento en X
 		if (xAxis != 0 && yAxis == 0) {
-			direction = playerDirection.isOnX;
-			animatorPlayer.SetBool ("isMoveSide", true);
+			direction = PlayerDirection.isOnX;
+			playerAnimator.SetBool ("isMoveSide", true);
 			if (xAxis > 0) {
 				desiredOrientation += Vector3.right;
 				this.transform.localScale = new Vector3(1, 1, 1);
@@ -44,28 +51,30 @@ public class Player : MovingEntity {
 				this.transform.localScale = new Vector3(-1, 1, 1);
 			}
 		} else {
-			animatorPlayer.SetBool ("isMoveSide", false);
+			playerAnimator.SetBool ("isMoveSide", false);
 		}
 		//Moviemiento en Y
 		if (yAxis != 0 && xAxis == 0) {
-			direction = playerDirection.isOnY;
+			direction = PlayerDirection.isOnY;
 			this.transform.localScale = new Vector3 (1, 1, 1);
 			if (yAxis > 0) {
 				desiredOrientation += Vector3.up;
-				animatorPlayer.SetBool ("isMoveUp", true);
+				playerAnimator.SetBool ("isMoveUp", true);
+				viewDirection = PlayerViewDirection.isViewUp;
 			} else {
 				desiredOrientation += Vector3.down;
-				animatorPlayer.SetBool ("isMoveDown", true);
+				playerAnimator.SetBool ("isMoveDown", true);
+				viewDirection = PlayerViewDirection.isViewDown;
 			}
 		} else {
-			animatorPlayer.SetBool ("isMoveUp", false);
-			animatorPlayer.SetBool ("isMoveDown", false);
+			playerAnimator.SetBool ("isMoveUp", false);
+			playerAnimator.SetBool ("isMoveDown", false);
 		}
 		//Movimiento en diagonal.
 		if (xAxis != 0 && yAxis != 0) {
 			desiredOrientation += new Vector3(xAxis, yAxis, 0);
-			if (direction == playerDirection.isOnX) {
-				animatorPlayer.SetBool ("isMoveSide", true);
+			if (direction == PlayerDirection.isOnX) {
+				playerAnimator.SetBool ("isMoveSide", true);
 				if (xAxis > 0) {
 					this.transform.localScale = new Vector3(1, 1, 1);
 				} else {
@@ -73,16 +82,49 @@ public class Player : MovingEntity {
 				}
 			} else {
 				if (yAxis > 0) {
-					animatorPlayer.SetBool ("isMoveUp", true);
+					playerAnimator.SetBool ("isMoveUp", true);
+					viewDirection = PlayerViewDirection.isViewUp;
 				} else {
-					animatorPlayer.SetBool ("isMoveDown", true);
+					playerAnimator.SetBool ("isMoveDown", true);
+					viewDirection = PlayerViewDirection.isViewDown;
 				}
 			}
 		}
 
 		desiredOrientation.Normalize();
 	}
-	
+
+	void Attack() {
+		//Atacar a los lados.
+		if (direction == PlayerDirection.isOnX && playerAnimator.GetBool("isAttackSide") == false) {
+			if (Input.GetKeyDown(KeyCode.Space)) {
+				playerAnimator.SetBool("isAttackSide", true);
+				Invoke("StopAttack",0.4f);
+			}
+		}
+		//Atacar hacia arriba o hacia abajo.
+		if (direction == PlayerDirection.isOnY) {
+			if (viewDirection == PlayerViewDirection.isViewUp && playerAnimator.GetBool("isAttackUp") == false) {
+				if (Input.GetKeyDown (KeyCode.Space)) {
+					playerAnimator.SetBool("isAttackUp", true);
+					Invoke("StopAttack",0.4f);
+				}
+			}
+			if (viewDirection == PlayerViewDirection.isViewDown && playerAnimator.GetBool("isAttackDown") == false) {
+				if (Input.GetKeyDown (KeyCode.Space)) {
+					playerAnimator.SetBool("isAttackDown", true);
+					Invoke("StopAttack",0.4f);
+				}
+			}
+		}
+	}
+
+	void StopAttack() {
+		playerAnimator.SetBool("isAttackSide", false);
+		playerAnimator.SetBool("isAttackUp", false);
+		playerAnimator.SetBool("isAttackDown", false);
+	}
+
 	void OnDestroy () {
 		if (OnDeath != null)
 			OnDeath(transform.position);
