15f34562a85a3491b0955e5e9b3fdd77d740d844
Armando <armandovmj@gmail.com>
2015-08-23 20:45:18 +0100

    score

--- unity-ludum/Assets/Scripts/Chick.cs
+++ unity-ludum/Assets/Scripts/Chick.cs
@@ -4,9 +4,13 @@ using System.Collections;
 public class Chick : MonoBehaviour {
 
     public static event FXManager.FxEvent OnDeath;
+    public static event ScoreManager.ScoreEvent RaiseScore;
 
     void OnDestroy() {
         SendMessageUpwards("SetChickensAngry", SendMessageOptions.RequireReceiver);
+        if (RaiseScore != null) {
+            RaiseScore(100);
+        }
         if (OnDeath != null) {
             OnDeath(transform.position);
         }
