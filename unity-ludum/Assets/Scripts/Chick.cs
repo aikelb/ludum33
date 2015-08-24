bacaf9a0dda1a1c2c2b2d1637f059d8ba8c7137f
Román <roman.diaz.leon@gmail.com>
2015-08-24 13:20:32 +0100

    A Comeeeeerrrrrr.

--- unity-ludum/Assets/Scripts/Chick.cs
+++ unity-ludum/Assets/Scripts/Chick.cs
@@ -5,12 +5,16 @@ public class Chick : MonoBehaviour {
 
     public static event FXManager.FxEvent OnDeath;
     public static event ScoreManager.ScoreEvent RaiseScore;
+	public static event NecklaceManager.NecklaceEvent RaiseNecklace;
 
     void OnDestroy() {
         SendMessageUpwards("SetChickensAngry", SendMessageOptions.RequireReceiver);
         if (RaiseScore != null) {
             RaiseScore(100);
         }
+		if (RaiseNecklace != null) {
+			RaiseNecklace(1);
+		}
         if (OnDeath != null) {
             OnDeath(transform.position);
         }
---
+++ unity-ludum/Assets/Scripts/NecklaceManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class NecklaceManager : MonoBehaviour {
+
+	public delegate void NecklaceEvent(int picks);
+
+	private int picksNumber;
+	private int currentPick;
+
+	// Use this for initialization
+	void Start () {
+		picksNumber = GameObject.FindObjectsOfType<Chick>().Length;
+		currentPick = 0;
+	}
+
+	void OnEnable() {
+		Chick.RaiseNecklace += Chick_RaiseNecklace;
+	}
+	
+	void OnDisable() {
+		Chick.RaiseNecklace -= Chick_RaiseNecklace;
+	}
+
+	void Chick_RaiseNecklace(int picks) {
+		AddPicksToNecklace (picks);
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void PrepareNecklace() {
+	}
+
+	void AddPicksToNecklace(int picks) {
+		for (int i = 0; i < picks; i++) {
+		}
+	}
+}
