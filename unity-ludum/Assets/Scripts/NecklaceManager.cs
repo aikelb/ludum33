ef0e7ab377d678580fb8dcc95dacf2472d93e79a
Román <roman.diaz.leon@gmail.com>
2015-08-24 16:24:19 +0100

    Sistema de picos aniadido.

--- unity-ludum/Assets/Scripts/NecklaceManager.cs
+++ unity-ludum/Assets/Scripts/NecklaceManager.cs
@@ -7,18 +7,22 @@ public class NecklaceManager : MonoBehaviour {
 	public delegate void NecklaceEvent(int picks);
 	public delegate void LevelCompleted();
 	public static event LevelCompleted OnLevelCompleted; 
-
-	public GameObject picoPrincipio;
-	public GameObject picoMedio1;
-	public GameObject picoMedio2;
+	public GameObject picoVacioPrincipio;
+	public GameObject picoVacioMedio1;
+	public GameObject picoVacioMedio2;
+	public GameObject picoLlenoPrincipio;
+	public GameObject picoLlenoMedio1;
+	public GameObject picoLlenoMedio2;
 
 	private int picksNumber;
 	private int currentPick;
+	private GameObject[] peaksNecklace;
 
 	// Use this for initialization
 	void Start () {
 		picksNumber = GameObject.FindObjectsOfType<Chick>().Length;
 		currentPick = 0;
+		peaksNecklace = new GameObject[picksNumber];
 		PrepareNecklace();
 	}
 
@@ -32,20 +36,42 @@ public class NecklaceManager : MonoBehaviour {
 
 	void Chick_RaiseNecklace(int picks) {
 		AddPicksToNecklace(picks);
-		currentPick += picks;
 		if (currentPick >= picksNumber && OnLevelCompleted != null) {
 			OnLevelCompleted();
 		} 
 	}
 
 	void AddPicksToNecklace(int picks) {
-
+		for (int i = 0; i < picks; i++) {
+			GameObject fullPeak;
+			if (currentPick == 0) {
+				fullPeak = GameObject.Instantiate(picoLlenoPrincipio) as GameObject;
+			} else {
+				if (currentPick%2 == 0) {
+					fullPeak = GameObject.Instantiate(picoLlenoMedio2) as GameObject;
+				} else {
+					fullPeak = GameObject.Instantiate(picoLlenoMedio1) as GameObject;
+				}
+			}
+			peaksNecklace[currentPick].GetComponent<Image>().sprite = fullPeak.GetComponent<Image>().sprite;
+			currentPick ++;
+		}
 	}
 
 	void PrepareNecklace() {
 		for (int i = 0; i < picksNumber; i++) {
-			GameObject p = GameObject.Instantiate(picoPrincipio) as GameObject;
-			p.transform.SetParent(transform);
+			GameObject emptyPeak;
+			if (i == 0) {
+				emptyPeak = GameObject.Instantiate(picoVacioPrincipio) as GameObject;
+			} else {
+				if (i%2 == 0) {
+					emptyPeak = GameObject.Instantiate(picoVacioMedio2) as GameObject;
+				} else {
+					emptyPeak = GameObject.Instantiate(picoVacioMedio1) as GameObject;
+				}
+			}
+			emptyPeak.transform.SetParent(transform);
+			peaksNecklace[i] = emptyPeak;
 		}
 	}
 }
